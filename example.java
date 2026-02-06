91250a45969016a0b1a9c2f981bc7f25648c77a4
bart-felds <bart.fieldsiii@gmail.com>
2026-02-06 12:38:06 -0500

    Create example.java

---
+++ example.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class example {
+
+ 
+    public static ArrayList<Integer> doubleListElements(ArrayList<Integer> originalList) {
+        ArrayList<Integer> doubledList = new ArrayList<>();
+
+        for (Integer element : originalList) {
+            doubledList.add(element);
+            doubledList.add(element); 
+        }
+
+        return doubledList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println("Original list: " + list);
+
+        ArrayList<Integer> doubledList = doubleListElements(list);
+        System.out.println("Doubled list:  " + doubledList);
+   
+    }
+}
